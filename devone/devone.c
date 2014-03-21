47f99a984c04cb6b58226816b1b5fd269cee60ff
Rafael do Nascimento Pereira <rnp@25ghz.net>
2014-03-21 15:58:58 +0100

    Added major and minor allocated
    Change-Id: I985e4fc4c7b763f461f07459c5ca127bafd004eb

--- devone/devone.c
+++ devone/devone.c
@@ -8,16 +8,13 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-
-#include <linux/kernel.h> /* printk() */
-#include <linux/slab.h> /* kmalloc() */
-#include <linux/fs.h> /* everything... */
-#include <linux/errno.h> /* error codes */
-#include <linux/types.h> /* size_t */
-#include <linux/proc_fs.h>
-#include <linux/fcntl.h> /* O_ACCMODE */
-#include <linux/seq_file.h>
 #include <linux/cdev.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/device.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Rafael do Nascimento Pereira");
@@ -27,9 +24,8 @@ MODULE_VERSION("0.1");
 #define NR_DEVS 1
 
 
-int devone_major = 0;
-int devone_minor = 0;
-
+static int devone_major = 0;
+static int devone_minor = 0;
 
 void devone_open(void)
 {
@@ -50,13 +46,25 @@ void devone_read(void)
 static int __init devone_init(void)
 {
 	int ret = 0;
+	dev_t dev = 0;
+
+	ret = alloc_chrdev_region(&dev, devone_minor, NR_DEVS, "devone");
+	devone_major = MAJOR(dev);
+
+	if (ret < 0) {
+		printk(KERN_WARNING "devone: can't get major %d\n", devone_major);
+		return ret;
+	}
 
 	return ret;
 }
 
 static void __exit devone_exit(void)
 {
+	dev_t dev;
+	dev = MKDEV(devone_major, devone_minor);
 
+	unregister_chrdev_region(dev, NR_DEVS);
 }
 
 module_init(devone_init);
