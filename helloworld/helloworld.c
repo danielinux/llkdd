32c3369ff589cf1127f99484c7bb223d2f7db586
Rafael do Nascimento Pereira <rnp@25ghz.net>
2014-03-25 10:03:09 +0100

    Added some comments to the helloworld driver
    Change-Id: I64b74e008f2a078274beabac1953fc18365ab489

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -3,6 +3,16 @@
 
 MODULE_LICENSE("GPL");
 
+/*
+ * This "device driver" is quite simple, it just sends a message using
+ * the printk function (see include/linux/kernel.h) when it is load and
+ * unloaded in the kernel space memory. You can see its output by typing
+ * as the root user:
+ * 
+ * syslog systems:  tail -f /var/log/syslog
+ *
+ * systemd systems: journalctl -f 
+ */
 static int hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
@@ -14,5 +24,9 @@ static void hello_exit(void)
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
+/*
+ * register kernel entry points for the device driver (aka constructor and
+ * destructor)
+ */
 module_init(hello_init);
 module_exit(hello_exit);
